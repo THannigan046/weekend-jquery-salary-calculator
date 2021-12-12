762a6a040628a4b4bb9e8ad80eec54eee416845e
Toivo Hannigan <THannigan046@gmail.com>
2021-12-12 14:08:45 -0600

    appending stuff to the dom now

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ Your project description goes here. What problem did you solve? How did you solv
 Technical tasks
 
 [] Project setup 
-    [] index.html
-    [] stylesheet
-    [] jquery
-    [] javascript script.js
-    [] doc.ready (console.log('JQ'))
-[] wireframe
-[] HTML mockup
+    [x] index.html
+    [x] stylesheet
+    [x] jquery
+    [x] javascript script.js
+    [x] doc.ready (console.log('JQ'))
+[x] wireframe
+[x] HTML mockup
 [] submit button
-    [] collect form info
+    [] collect form info from <input>
     [] store info to calculate monthly costs
     [] append info to the DOM
     [] Clear input fields
@@ -24,6 +24,15 @@ Technical tasks
     [] if totalMonthlyCost > 20k, add a red background
 [] Delete button
     [] removes an employee from the dom. (similar to the flower list thing)
-[] Basic styling, so it's not awful (boostrap maybe)
+[] Basic styling, so it's not awful (boostrap, maybe)
 
+company = [
+    employee: {
+    firstName:
+    lastName:
+    id:
+    title:
+    annualSalary:
+}
+]
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- app.js
+++ app.js
@@ -2,6 +2,55 @@ console.log('js');
 
 $(document).ready(onReady)
 
+let workplace = []
 function onReady() {
     console.log('JQ');
+
+$('#employeeForm').on('submit', onAddEmployee)
 };
+function onAddEmployee(event){
+    //prevent reload 
+
+    event.preventDefault()
+    console.log('employee added');
+
+    //grab stuff from DOM
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let id = Number($('#idInput').val());
+    let title = $('#titleInput').val();
+    let salary = Number($('#salaryInput').val());
+    
+    //put it in an object
+    let employee = {
+        firstName: firstName,
+        lastName: lastName,
+        id: id,
+        title: title,
+        salary: salary
+    }
+    console.log('new employee', employee);
+    workplace.push(employee);
+    console.log('workplace', workplace);
+    for (let employee of workplace){
+        console.log('employee in loop', employee);
+        $('#employeeList').append(`
+        <tr>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+                <td>${employee.id}</td>
+                <td>${employee.title}</td>
+                <td>${employee.salary}</td>
+                <td>
+                    <button class="deleteBtn">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        `
+       );
+    }
+    
+}
+
+
